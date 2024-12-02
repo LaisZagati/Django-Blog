cd8ff515f8499deec6da2b6fb85a4d43c762fd7f
LaisZagati <laisviana97@gmail.com>
2024-12-02 21:01:50 +0000

    deployment

---
+++ env.py
@@ -0,0 +1,11 @@
+import os
+# from supabase import create_client, Client
+
+
+
+os.environ ["DATABASE_URL"] = "https://deodelifiqscswmgwccg.supabase.co"
+os.environ ["SECRET_KEY"] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlb2RlbGlmaXFzY3N3bWd3Y2NnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMzEzMDg2NCwiZXhwIjoyMDQ4NzA2ODY0fQ.NJ-ZCt0Lzm9qdcpKmBWXMgLDYs-YUBxrmfXJ6uNhJUI"
+os.environ ["CLOUDINARY_URL"] = "cloudinary://785893614844633:pnaLwItOoeV_dAGB6XNvVKUREZk@dran595pk"
+    
+# supabase: Client = create_client(url, key)
+
