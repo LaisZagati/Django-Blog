69a5c50d2cb7fe3f97f2248fbf9fb8fbf104e06d
LaisZagati <laisviana97@gmail.com>
2024-12-05 20:54:07 +0000

    Created classes/features for Posts and comments in the Django Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,34 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     
+    list_display = ('title', 'slug', 'created_on', 'status')
+    search_fields = ['title','content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
+   
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name','email','body']
+    actions = ['approved_comments']
+    
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+        
+    
+    
+    
+     
+    
 
 
 
